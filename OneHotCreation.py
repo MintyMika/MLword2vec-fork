9cde674ac4f34edf02dccb50e8190838a5f34ec6
Stephen C <83474041+MintyMika@users.noreply.github.com>
2024-04-29 15:41:50 -0500

    Dr. Dutter Sesh

--- OneHotCreation.py
+++ OneHotCreation.py
@@ -2,6 +2,6 @@ import torch
 
 # Creating 19970 vectors of size 19970 with a one in the index of the artist and then saving them as tensors to a file
 vectors = torch.eye(19970)
-print(vectors[2])
-#torch.save(vectors, 'OneHot.pt')
+# print(vectors[2])
+torch.save(vectors, 'OneHot.pt')
 
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 # Load the JSON data into a Python dictionary
-with open('artistValues.json', 'r') as f:
+with open(r'data\artistValues.json', 'r') as f:
     data = json.load(f)
 
 # Function to find the number associated with the given name
@@ -13,33 +13,40 @@ def find_number(name):
     else:
         return None  # Return None if name not found
 
-#load model
-class YourModel(nn.Module):
-    def __init__(self):
-        super(YourModel, self).__init__()
-        # Define your model layers here
-
-    def forward(self, x):
-        # Define the forward pass of your model
-        return x
 
 
-name = input("Enter a name: ")
-number = find_number(name)
-if number is not None:
-    # Load the one-hot encoding
-    one_hot_vectors = torch.load('OneHot.pt')
 
-    # Specify the index of the vector
-    index = number
+matdata = torch.load(r'data\model.pth')
 
-    # Check if the specified index is within the range of the loaded vectors
-    if index < len(one_hot_vectors):
-        # Print the one-hot encoding vector at the specified index
-        print("One-hot encoding vector at index", index, ":", one_hot_vectors[index])
-    else:
-        print("Index out of range.")
+neuralNet = nn.Embedding(num_embeddings=19971, embedding_dim=70)
+neuralNet.load_state_dict(torch.load(r'data\model.pth'))
+neuralNet.eval()
+# Save weights
+torch.save(neuralNet.embed.weight, r'data\weights.pth')
 
-else:
-    print(f"Name '{name}' not found in the data.")
+artist1 = 24
+artist2 = 28
+artist3 = 27
 
+# myVec will be the matdata[artist1] + matdata[artist2] + matdata[artist3] and then average
+myVec = (matdata[artist1] + matdata[artist2] + matdata[artist3]) / 3
+
+# now matdata * myVec
+something = torch.matmul(matdata, myVec)
+
+# divide by sqrt(sum of squares of matdata)
+saveLater = torch.norm(matdata, dim=1)
+
+# something / saveLater
+cosineSimilarity = torch.div(something, saveLater)
+
+# get the top 10
+top10 = torch.topk(cosineSimilarity, 10)
+
+print(top10.indices)
+similarArtists = []
+for i in top10.indices:
+    for key, value in data.items():
+        if value == i:
+            similarArtists.append(key)
+            print(key)
